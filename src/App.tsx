5882210de1d45b8b6e8b38766f94a504b29610c4
srikar-codehub <a.srikar.code@gmail.com>
2023-11-06 11:04:22 +0530

    devlivery markers and delivery coordinates added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import React, { useEffect, useRef, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import * as tt from "@tomtom-international/web-sdk-maps";
+import * as ttapi from "@tomtom-international/web-sdk-services";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
-
-// import tt, { MapOptions } from "@tomtom-international/web-sdk-maps";
-
-// type Map = {
-//   remove(): void;
-// };
 
 /////start////////
 const App = () => {
   const mapElement = useRef<HTMLDivElement>(null); ////USE REF MAP ELEMENT
-  const [map, setMap] = useState({}); //// MAP STATE
+  const [Map, setMap] = useState<tt.Map | null>(); //// MAP STATE
   const [longitude, setLongitude] = useState<number>(78.48014548283436);
   const [latitude, setLatitude] = useState<number>(17.362464103232327);
 
   ////USE EFFECT FOR MAP COMPONENT ON MOUNT
-  /////INTIALIZE MAP
+  /////INTIALIZE MAP/////////////
   useEffect(() => {
+    let mapInstance: tt.Map | null = null;
+
+    /////Loading map Instance//////////
     if (mapElement.current) {
-      let map = tt.map({
+      mapInstance = tt.map({
         key: process.env.REACT_APP_TOM_TOM_API as string,
         container: mapElement.current,
         stylesVisibility: {
@@ -31,44 +28,96 @@ const App = () => {
         center: [longitude, latitude],
         zoom: 14,
       });
+
+      ///ON LOADING MAP//////////////////
+      //////////POPUP///////////
+      ////////Marker///////////////
+
+      mapInstance.on("load", () => {
+        setMap(mapInstance);
+
+        ////POPUP/////////////
+        const popup = new tt.Popup({
+          offset: { bottom: [0, -50] },
+        }).setHTML("popup");
+
+        /////MARKER/////////
+        const markerElement: HTMLDivElement = document.createElement("div");
+        markerElement.className = "marker";
+
+        const marker = new tt.Marker({
+          draggable: true,
+          element: markerElement,
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(mapInstance!);
+        marker.on("dragend", () => {
+          const lnglat = marker.getLngLat();
+          setLongitude(lnglat.lng);
+          setLatitude(lnglat.lat);
+        });
+        marker.setPopup(popup).togglePopup();
+      });
+
+      /////delivery marker/////////
+      const addDeliveryMarker = (lnglat: tt.LngLat) => {
+        const deliveryMarker = document.createElement("div");
+        deliveryMarker.className = "markerDelivery";
+        new tt.Marker({
+          element: deliveryMarker,
+        })
+          .setLngLat(lnglat)
+          .addTo(mapInstance!);
+      };
+
+      const destinations: tt.LngLat[] = [];
+      mapInstance?.on("click", (e) => {
+        destinations.push(e.lngLat);
+
+        addDeliveryMarker(e.lngLat);
+      });
+      return () => mapInstance?.remove();
     }
-    setMap(map);
   }, [latitude, longitude]);
 
   //////HTML ELEMENTS////////////
   return (
-    <div className="App">
-      <div ref={mapElement} className="map"></div>
-      <div className="searchBar">
-        <h4>Enter Co-ordinates</h4>
-        <input
-          type="text"
-          id="longitude"
-          className="longitude"
-          onChange={(e) => {
-            const value = parseFloat(e.target.value);
-            if (!isNaN(value)) {
-              setLongitude(value);
-            } else {
-              setLongitude(78.48014548283436);
-            }
-          }}
-        />
-        <input
-          type="text"
-          id="latitude"
-          className="latitude"
-          onChange={(e) => {
-            const value = parseFloat(e.target.value);
-            if (!isNaN(value)) {
-              setLatitude(value);
-            } else {
-              setLatitude(17.362464103232327);
-            }
-          }}
-        />
+    <>
+      <div className="App">
+        <div ref={mapElement} className="map"></div>
+        <div className="searchBar">
+          <h4>Enter Co-ordinates</h4>
+          <input
+            type="text"
+            id="longitude"
+            className="longitude"
+            placeholder="longitude"
+            onChange={(e) => {
+              const value = parseFloat(e.target.value);
+              if (!isNaN(value)) {
+                setLongitude(value);
+              } else {
+                setLongitude(78.48014548283436);
+              }
+            }}
+          />
+          <input
+            type="text"
+            id="latitude"
+            className="latitude"
+            placeholder="latitude"
+            onChange={(e) => {
+              const value = parseFloat(e.target.value);
+              if (!isNaN(value)) {
+                setLatitude(value);
+              } else {
+                setLatitude(17.362464103232327);
+              }
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
